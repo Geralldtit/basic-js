fd5ff789c296555b3cb308efe8940a635b0fcd31
geralldtit <geralldtit@gmail.com>
2020-03-19 00:20:32 +0300

    feat: implement Dream-team subtask

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,9 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members) || members.length == 0)
+    return false;
+
+  return members.filter((item)=> typeof item === 'string')
+                .map((item) => item.trim().toUpperCase()[0])
+                .sort()
+                .join('');
 };

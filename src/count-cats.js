fb8206110127ee667296ae3a971a07cb0b9d065c
geralldtit <geralldtit@gmail.com>
2020-03-18 23:56:17 +0300

    feat: implement Count-cats subtask

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let number = 0;
+  for(let k = 0; k < matrix.length; k++)
+    for(let m = 0; m < matrix[k].length; m++){
+      if(matrix[k][m] === "^^")
+        number++;
+    }
+
+
+  return number;
 };

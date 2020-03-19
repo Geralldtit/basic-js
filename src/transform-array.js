57edf9e83672633e540f276d27d445401eafb19f
geralldtit <geralldtit@gmail.com>
2020-03-19 18:33:53 +0300

    init: transform-array subtask

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,29 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) throw 'Error: input parameter should be array!';
+
+    for( let i = 0; i < arr.length; i++){
+      if(typeof arr[i] === 'string' || arr[i] instanceof String){
+        switch(arr[i]){
+          case '--discard-next':
+            if(i < arr.length-1)
+              arr.splice(i+1, 1);
+            break;
+          case '--discard-prev':
+            if(i != 0)
+              arr.splice(i-1, 1);
+            break;
+          case '--double-next':
+            if(i < arr.length-1 && Number(arr[i+1]))
+              arr[i+1] *= 2;
+            break;
+          case '--double-prev':
+            if(i != 0 && Number(arr[i-1]))
+              arr[i-1] *= 2;
+            break;
+        }
+        arr.splice(i, 1);
+      }
+    }
+
+    return arr;
 };

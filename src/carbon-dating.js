ec18b454abc2e4fe5fc5b1ad7b0230202aa26884
geralldtit <geralldtit@gmail.com>
2020-03-20 22:53:03 +0300

    feat: implement Carbon-dating subtask

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+// 1 => 22392
+
+module.exports = function dateSample(sampleActivity) {
+  let num = parseFloat(sampleActivity);
+  if((typeof(sampleActivity) != 'string') ||
+    !isFinite(num) ||
+    (num >= MODERN_ACTIVITY)||
+    (num <= 0))
+      return false;
+
+  const t = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / num) / t);
 };

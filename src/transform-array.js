90703966db3cdd9da176820ae9dfb514b1ec20b8
geralldtit <geralldtit@gmail.com>
2020-03-20 18:47:37 +0300

    fix: part of algorithm for Transform-array subtask

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,29 +1,44 @@
-module.exports = function transform(arr) {
-    if(!Array.isArray(arr)) throw 'Error: input parameter should be array!';
+module.exports = function transform(arr, expected, shouldPrint) {
+  if(!Array.isArray(arr)) throw 'Error: input parameter should be array!';
 
-    for( let i = 0; i < arr.length; i++){
-      if(typeof arr[i] === 'string' || arr[i] instanceof String){
-        switch(arr[i]){
-          case '--discard-next':
-            if(i < arr.length-1)
-              arr.splice(i+1, 1);
-            break;
-          case '--discard-prev':
-            if(i != 0)
-              arr.splice(i-1, 1);
-            break;
-          case '--double-next':
-            if(i < arr.length-1 && Number(arr[i+1]))
-              arr[i+1] *= 2;
-            break;
-          case '--double-prev':
-            if(i != 0 && Number(arr[i-1]))
-              arr[i-1] *= 2;
-            break;
-        }
-        arr.splice(i, 1);
+  if(shouldPrint){
+    console.log('============input===========');
+    for(let k = 0 ; k < arr.length; k++)
+      console.log(arr[k]+', ');
+    console.log('============expected=============');
+    for(let k = 0 ; k < expected.length; k++)
+      console.log(expected[k]+', ');
+  }
+
+  let outputArr = [...arr];
+  let isCommand = false;
+  for( let q = 0; q < arr.length; q++){
+    if(typeof arr[q] === 'string' || arr[q] instanceof String){
+      switch(arr[q]){
+        case '--discard-next':
+          if(q < arr.length-1)
+            outputArr[q+1] = '--discard-next';
+          break;
+        case '--discard-prev':
+          if(q > 0)
+            outputArr[q-1] = '--discard-prev';
+          break;
+        case '--double-next':
+          if(q < arr.length-1){
+            outputArr.splice(q+1, 0, arr[q+1]);
+            outputArr.splice(q, 1);
+          }
+          break;
+        case '--double-prev':
+          if(q > 0 ){
+            outputArr.splice(q, 0, arr[q-1]);
+            outputArr.splice(q+1, 1);
+          }
+          break;
       }
     }
+  }
 
-    return arr;
+  let result = outputArr.filter((item) => (typeof item != 'string')|| ((typeof item === 'string') && (item[0]!='-')) );
+  return result;
 };
